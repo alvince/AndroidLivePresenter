10dd17e6d881ead1f37a4a608f5f5dc7a9e65af6
alvince <alvince.zy@gmail.com>
2018-12-03 14:52:57 +0800

    Add library dependency to sample module

---
+++ sample/src/main/java/me/alvince/arch/livepresenter/sample/home/presenter/MainPresenter.kt
@@ -0,0 +1,12 @@
+package me.alvince.arch.livepresenter.sample.home.presenter
+
+import me.alvince.arch.livepresenter.LivePresenter
+import me.alvince.arch.livepresenter.sample.home.IHomeView
+
+/**
+ * Created by alvince on 2018/12/3
+ *
+ * @author alvince.zy@gmail.com
+ */
+class MainPresenter : LivePresenter<IHomeView>() {
+}
